b5dc04d889ac8b80872d80ad4093d15635598c98
Jingyu <jingyu.niu@gmail.com>
2021-04-09 15:55:04 +0800

    CU-7ny6ah - check the controller should not changed when publish(update) the customized DID

---
+++ src/main/java/org/elastos/did/exception/DIDControllersChangedException.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2019 Elastos Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.elastos.did.exception;
+
+/**
+ * Unchecked exception thrown when an attempt is made to publish a DID
+ * that the controllers changed.
+ */
+public class DIDControllersChangedException extends IllegalStateException {
+	private static final long serialVersionUID = 6178201715662640795L;
+
+	/**
+	 * Constructs a DIDControllersChangedException with null as its error
+	 * detail message.
+	 */
+	public DIDControllersChangedException() {
+		super();
+	}
+
+	/**
+	 * Constructs a DIDControllersChangedException with the specified
+	 * detail message.
+	 *
+	 * @param message The detail message
+	 */
+	public DIDControllersChangedException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a DIDControllersChangedException with the specified detail
+	 * message and cause.
+	 *
+	 * Note that the detail message associated with cause is not automatically
+	 * incorporated into this exception's detail message.
+	 *
+	 * @param message The detail message
+	 * @param cause The cause. A null value is permitted, and indicates
+	 * 			that the cause is nonexistent or unknown
+	 */
+	public DIDControllersChangedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a DIDControllersChangedException with the specified cause
+	 * and a detail message from that cause.
+	 *
+	 * @param cause The cause. A null value is permitted, and indicates
+	 * 			that the cause is nonexistent or unknown
+	 */
+	public DIDControllersChangedException(Throwable cause) {
+		super(cause);
+	}
+}
